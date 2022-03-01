45d9ac7a8af9acaf8152a724bb81edf6084a715d
Carlos Antônio <95361309+carlosantonio212@users.noreply.github.com>
2022-03-01 13:38:47 +0000

    percentual desejado

---
+++ percentual desejado.py
@@ -0,0 +1,7 @@
+print ("Digite um número:")
+num1 = float(input())
+
+print("Digite um percentual desejado para seu número escolhido:")
+percentual = float(input())
+
+print(" O percentual do número desejado foi:", num1 * percentual / 100)
