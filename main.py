d0738c80317fa867721394fdedda2a34b35ac08a
Carlos Antônio <95361309+carlosantonio212@users.noreply.github.com>
2022-03-15 11:58:15 +0000

    new

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Por favor digite quanto voçe ganha por hora trabalhada: \n ") 
+
+ # O código acima solicita ao usuário informações
+
+  salario_hora = float(input())
+
+ # O código acima armazena o valor informado pelo usuário
+
+print("Digite quantas horas você trabalha durante o mês: \n")
+
+ # O código acima solicita ao usuário informações
+
+  hora = float(input())
+
+ # O código acima armazena o valor informado pelo usuário
+
+  salario = (salario_hora * hora)
+
+ # O código acima faz o cálculo do salário das horas multiplicado pelas horas
+
+print("O total do seu salario recebido pelas suas horas trabalhadas no mês é :",salario, "reais ")
+
+ # O código acima exibe o resultado do salario pelas horas trabalhadas que o usúario espera como retorno
--- ola.py
+++
@@ -1,7 +0,0 @@
-print('Olá mundo')
-print('carlos')
-num1 = 2 + 2
-num2 = 3 + 3
-total = num1 + num2
-print(total)
-print(num1, num2, total)
